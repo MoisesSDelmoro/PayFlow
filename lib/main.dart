3243fa361d805b366187da8a52b77fb5fe0f24f0
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-06-22 14:42:55 -0300

    Telas Sphash e Login prontas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
@@ -13,7 +14,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
