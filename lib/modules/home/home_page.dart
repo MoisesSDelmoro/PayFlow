429bdf664ced6ff693df7e7f31503ff142a37667
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-06-24 10:29:45 -0300

    aula 3

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +27,73 @@ class HomePage extends StatelessWidget {
         preferredSize: Size.fromHeight(152),
         child: Container(
           height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: AppTextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                        text: "Moisés",
+                        style: AppTextStyles.titleBoldBackground),
+                  ],
+                ),
+              ),
+              subtitle: Text("Mantenha suas contas em dia",
+                  style: AppTextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                controller.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              ),
+            ),
+            Container(
+              height: 56,
+              width: 56,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: IconButton(
+                onPressed: () {
+                  controller.setPage(1);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.description_outlined, color: AppColors.body),
+            ),
+          ],
         ),
       ),
     );
